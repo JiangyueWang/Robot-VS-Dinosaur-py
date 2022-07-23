ddfeec43e2a46bfee06d715b5e6c1eb927af47af
Jiang <jiangyue0380@gmail.com>
2022-07-23 21:14:20 +1000

    updated attack method in dinosaur.py

--- dinosaur.py
+++ dinosaur.py
@@ -7,8 +7,5 @@ class Dinosaur:
 
     def attack(self, robot):
         robot.health = robot.health - self.attack_power
-        if robot.health > 0:
-            print(
+        print(
                 f'\n{self.name} attacked {robot.name} with a Power Loader for {self.attack_power}')
-        else:
-            return None
